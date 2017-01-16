c25a5c50a1ec95b80ceb5cdb49af3e7b3e702909
Timothy Chung <timchunght@gmail.com>
2017-01-16 17:20:16 -0500

    added initial demo

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	"log"
+	"os"
+)
+
+type TheatreWaitingList struct {
+	gorm.Model
+	ID                        int    `gorm:"ID"`
+	StatusID                  int    `gorm:"STATUS_ID"`
+	OfferedOutcomeDescription string `gorm:"OFFERED_OUTCOME_DESCRIPTION"`
+}
+
+func main() {
+
+	user := os.Getenv("MS_USER")
+	host := os.Getenv("MS_HOST")
+	password := os.Getenv("MS_PASSWORD")
+	dbName := os.Getenv("MS_DB")
+	dbConnectionStr := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", host, user, dbName, password)
+	log.Println("dbConnectionStr: ", dbConnectionStr)
+	db, err := gorm.Open("mssql", dbConnectionStr)
+	if err != nil {
+		log.Println(err)
+	}
+
+	rows, err := db.Model(&TheatreWaitingList{}).Raw("SELECT TOP 10 * FROM dbo.TheatreWaitingList").Rows() // (*sql.Rows, error)
+	for rows.Next() {
+		var twl TheatreWaitingList
+		db.ScanRows(rows, &twl)
+		log.Println(twl)
+		// do something
+	}
+
+	defer db.Close()
+
+}
